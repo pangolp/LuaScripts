f6d930b77bbebce49c20b3a5ac9ffcc7e56e0a47
Pagani Walter <paganiwalter@gmail.com>
2020-01-03 17:26:28 -0300

    fixeando error en registros del sql de npc en creature_template

--- quest-clase/sql/npc_clases.sql
+++ quest-clase/sql/npc_clases.sql
@@ -3,7 +3,7 @@ SET @GUID:=400000;
 
 DELETE FROM `creature_template` WHERE `entry`=@ENTRY;
 INSERT INTO `creature_template` (`entry`,`difficulty_entry_1`,`difficulty_entry_2`,`difficulty_entry_3`,`KillCredit1`,`KillCredit2`,`modelid1`,`modelid2`,`modelid3`,`modelid4`,`name`,`subname`,`IconName`,`gossip_menu_id`,`minlevel`,`maxlevel`,`exp`,`faction`,`npcflag`,`speed_walk`,`speed_run`,`scale`,`rank`,`mindmg`,`maxdmg`,`dmgschool`,`attackpower`,`DamageModifier`,`BaseAttackTime`,`RangeAttackTime`,`unit_class`,`unit_flags`,`unit_flags2`,`dynamicflags`,`family`,`trainer_type`,`trainer_spell`,`trainer_class`,`trainer_race`,`minrangedmg`,`maxrangedmg`,`rangedattackpower`,`type`,`type_flags`,`lootid`,`pickpocketloot`,`skinloot`,`resistance1`,`resistance2`,`resistance3`,`resistance4`,`resistance5`,`resistance6`,`spell1`,`spell2`,`spell3`,`spell4`,`spell5`,`spell6`,`spell7`,`spell8`,`PetSpellDataId`,`VehicleId`,`mingold`,`maxgold`,`AIName`,`MovementType`,`InhabitType`,`HoverHeight`,`HealthModifier`,`ManaModifier`,`ArmorModifier`,`RacialLeader`,`movementId`,`RegenHealth`,`mechanic_immune_mask`,`flags_extra`,`ScriptName`,`VerifiedBuild`) VALUES
-(@ENTRY,0,0,0,0,0,29615,0,0,0,"Chuck Norris","Los indestructibles","Speak",5000,80,80,0,35,1,1,0,0.5,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"",0,0,0,0,0,0,0,0,0,0,0,"",1);
+(@ENTRY,0,0,0,0,0,29615,0,0,0,"ChuckNorris","Losindestructibles","Speak",5000,80,80,0,35,1,1,1.14286,0.5,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"",0,1,0,0,0,0,0,0,0,0,0,"",1);
 
 DELETE FROM `creature` WHERE `id`=@ENTRY AND `guid` BETWEEN @GUID+0 AND @GUID+7;
 
