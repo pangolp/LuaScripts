cac73406cbe2392ae6f638355dfbebf27f4290f6
Pagani Walter <paganiwalter@gmail.com>
2020-01-09 13:18:05 -0300

    Agregando reputaciones faltantes

--- quest-reputacion/lua/npc_misiones_rasganorte.lua
+++ quest-reputacion/lua/npc_misiones_rasganorte.lua
@@ -16,10 +16,28 @@ function OnGossipHello(event, player, object)
 				if (player:GetQuestRewardStatus(12898) == false) then
 					player:GossipMenuAddItem(0, '¡Los caballeros de la Espada de Ébano!', 1, 10)
 				end
+				if (player:GetReputation(1090) ~= 42999) then
+					player:GossipMenuAddItem(0, '¡Kirin Tor!', 1, 11)
+				end
+				if (player:GetReputation(1091) ~= 42999) then
+					player:GossipMenuAddItem(0, '¡El Acuerdo del Reposo del Dragón!', 1, 12)
+				end
+				if (player:GetReputation(1156) ~= 42999) then
+					player:GossipMenuAddItem(0, '¡El Veredicto Cinéreo!', 1, 13)
+				end
 			else
 				-- Si el jugador es horda…
 				if (player:GetQuestRewardStatus(12899) == false) then
 					player:GossipMenuAddItem(0, '¡Los caballeros de la Espada de Ébano!', 1, 10)
+				end
+				if (player:GetReputation(1090) ~= 42999) then
+					player:GossipMenuAddItem(0, '¡Kirin Tor!', 1, 11)
+				end
+				if (player:GetReputation(1091) ~= 42999) then
+					player:GossipMenuAddItem(0, '¡El Acuerdo del Reposo del Dragón!', 1, 12)
+				end
+				if (player:GetReputation(1156) ~= 42999) then
+					player:GossipMenuAddItem(0, '¡El Veredicto Cinéreo!', 1, 13)
 				end
 			end
 		end
@@ -42,7 +60,25 @@ function OnGossipSelect(event, player, object, sender, intid, code, menuid)
 				player:RewardQuest(quest)
 			end
 		end
+		player:SetReputation(1098, 42999)
+
 	end -- Misiones de la espalda de ebano.
+	if (intid == 11) then
+		-- En el caso del Kirin Tor, lo subimos directamente a exaltado.
+		-- No requiere de misiones para ver al intendente, se encuentra en dalaran.
+		player:SetReputation(1090, 42999)
+	end
+	if (intid == 12) then
+		-- Mismo caso que el de kirin tor. 
+		-- No es necesario completar misiones para poder localizar al intendente en lo alto de la torre de cementerio de dragones.
+		player:SetReputation(1091, 42999)
+	end
+	if (intid == 13) then
+		-- Mismo caso que el de kirin tor y reposo del dragón
+		player:SetReputation(1156, 42999)
+	end
+	
+	player:GossipComplete()
 end
 
 RegisterCreatureGossipEvent(npcEntry, 1, OnGossipHello)
